aab66db42b9d4c24b458907bdd70ea3d01e9f5ee
adamY97 <adam24891@gmail.com>
2021-10-20 15:08:13 +0200

    Initial commit

---
+++ src/main/java/com/example/libreriaoperaciones/Operaciones.java
@@ -0,0 +1,5 @@
+package com.example.libreriaoperaciones;
+
+public class Operaciones {
+
+}
---
+++ src/test/java/com/example/libreriaoperaciones/LibreriaOperacionesApplicationTests.java
@@ -0,0 +1,13 @@
+//package com.example.libreriaoperaciones;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class LibreriaOperacionesApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
